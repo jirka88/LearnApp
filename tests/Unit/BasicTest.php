c8c454a3b115ab0ebb0508dc1004b115fca6bfe0
jirka88 <laval@centrum.cz>
2024-01-21 17:03:38 +0100

    unit

---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class BasicTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
